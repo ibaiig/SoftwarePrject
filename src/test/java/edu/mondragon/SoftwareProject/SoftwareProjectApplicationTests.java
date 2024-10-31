f11340e14dda9719d43812f04c648ce1cad4841e
ibaiig <ibai.goni@alumni.mondragon.edu>
2024-10-31 08:29:50 +0100

    Maven project creation

---
+++ src/test/java/edu/mondragon/SoftwareProject/SoftwareProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mondragon.SoftwareProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7a6400cedfb1b2f979107ff1c7a97894857ce5d7
ibaiig <ibai.goni@alumni.mondragon.edu>
2024-10-31 09:52:19 +0100

    Movimiento

---
+++ src/test/java/edu/mondragon/SoftwareProject/TestMovimiento.java
@@ -0,0 +1,14 @@
+package edu.mondragon.SoftwareProject;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+public class TestMovimiento {
+    Movimiento movimiento;
+    @Test
+    public void TestMovimiento(){
+        movimiento = new Movimiento(1,2);
+        movimiento.setPosX(movimiento.getPosY());
+        movimiento.setPosY(movimiento.getPosX());
+        assertEquals(movimiento.getPosY(),2);
+    }
+}
